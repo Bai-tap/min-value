16b4df02f3593061141a159ae53484c88b8aa452
longsutls <longsutls@gmail.com>
2018-08-13 21:30:07 +0700

    Add method main and display min value

--- src/MinValue.java
+++ src/MinValue.java
@@ -1,4 +1,14 @@
 public class MinValue {
+    public static void main(String[] args) {
+        int[] array = {29, 30, 51, 59, 9, 888, 123, 4};
+        System.out.println("Element of array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        int index = minValue(array);
+        System.out.println("\nMin value is: " + array[index]);
+    }
+
     public static int minValue(int array[]) {
         int min = array[0];
         int index = 0;
